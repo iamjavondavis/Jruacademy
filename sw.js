613826969fe749c6afbc9f1187394b4120c91e55
iamjavondavis <javon.davis21@gmail.com>
2026-07-12 18:18:40 -0700

    Add offline cache service worker for Jru Academy

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE='jru-academy-v2';
+const CORE=[
+  './','./index.html','./manifest.webmanifest','./assets/jru-academy-icon.svg',
+  './data/math-lessons.js','./data/reading-lessons.js','./data/quick-challenges.js',
+  './data/roblox-course.js','./data/academy-expansion.js','./data/ui-upgrade.js',
+  './data/avatar-studio.js','./data/kid-language.js','./data/creator-academy.js','./data/app-install.js'
+];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    if(new URL(event.request.url).origin===location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
