8b955456f5b0f97f623bb809b6412103639a09c1
iamjavondavis <javon.davis21@gmail.com>
2026-07-12 20:36:16 -0700

    Cache Block Focus polish update

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE='jru-academy-v3';
+const CACHE='jru-academy-v4';
 const CORE=[
   './','./index.html','./manifest.webmanifest','./assets/jru-academy-icon.svg',
   './data/math-lessons.js','./data/reading-lessons.js','./data/quick-challenges.js',
   './data/roblox-course.js','./data/academy-expansion.js','./data/ui-upgrade.js',
   './data/avatar-studio.js','./data/kid-language.js','./data/creator-academy.js',
-  './data/app-install.js','./data/music-workflow.js','./data/roblox-3d-lab.js'
+  './data/app-install.js','./data/music-workflow.js','./data/roblox-3d-lab.js',
+  './data/block-focus-polish.js'
 ];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
